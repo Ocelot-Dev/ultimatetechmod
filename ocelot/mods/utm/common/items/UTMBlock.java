d823dda73650972d8a3c55db870ef123d9795fe3
Peter <peter@werl.me>
2013-08-14 19:48:41 -0600

    started blocks

---
+++ ocelot/mods/utm/common/items/UTMBlock.java
@@ -0,0 +1,14 @@
+package ocelot.mods.utm.common.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class UTMBlock extends Block
+{
+
+	public UTMBlock(int id, Material material)
+	{
+		super(id, material);
+	}
+
+}
