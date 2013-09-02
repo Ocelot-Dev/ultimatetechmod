1df510c4e21a13086ddcffa9a8bd6e5a4c8c3b15
Peter <peter@werl.me>
2013-09-01 21:10:30 -0600

    check to see if it is not raining and it can see the sky

--- ocelot/mods/utm/common/entity/TilePrototypeSolarFurnace.java
+++ ocelot/mods/utm/common/entity/TilePrototypeSolarFurnace.java
@@ -2,15 +2,42 @@ package ocelot.mods.utm.common.entity;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class TilePrototypeSolarFurnace extends TileInventory
 {
+	private boolean canWork = false;
 
 	public TilePrototypeSolarFurnace(int invSize)
 	{
 		super(invSize);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void updateEntity() 
+	{
+		if(!this.worldObj.isRaining() && this.worldObj.isDaytime() && this.worldObj.canBlockSeeTheSky(this.xCoord, this.yCoord, this.zCoord))
+		{
+			canWork = true;
+		}
+		else
+			canWork = false;
+	}
+	
+	public void readFromNBT(NBTTagCompound tag)
+    {
+        super.readFromNBT(tag);
+        canWork = tag.getBoolean("seeSky");
+    }
+
+    /**
+     * Writes a tile entity to NBT.
+     */
+    public void writeToNBT(NBTTagCompound tag)
+    {
+        super.writeToNBT(tag);
+        tag.setBoolean("seeSky", this.canWork);
+    }
 
 	@Override
 	public void setInventorySlotContents(int i, ItemStack itemstack)
